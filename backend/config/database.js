453277e36065e675d230b321c4b7127f0604bc40
jeevhub <95094325+jeevhub@users.noreply.github.com>
2021-12-14 00:40:15 +0000

    Config to read db creds from env file

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,10 @@
 import { Sequelize } from "sequelize";
- 
-const db = new Sequelize('employee', 'admin', '', {
-    host: "localhost",
+import { DotEnv } from "dotenv" 
+
+const config = DotEnv.config();
+
+const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER_NAME , process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: "mysql"
 });
  
